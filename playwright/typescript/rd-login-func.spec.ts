7762ff6a1ea2f4d272b42fb3d3a8ca3605c1b835
Ryan Docherty <ryandocherty95@gmail.com>
2025-06-16 14:15:49 +0100

    Changes to be committed: 	modified:   playwright/typescript/rd-login-func.spec.ts     >Test to verify logging in as an existing user using an API call.

--- playwright/typescript/rd-login-func.spec.ts
+++ playwright/typescript/rd-login-func.spec.ts
@@ -1,11 +1,39 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, request } from "@playwright/test";
 import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-const rd_loginEmail = process.env.rd_email_address;
-const rd_password = process.env.rd_password;
+const rd_email: any = process.env.rd_email_address ?? "";
+const rd_password: any = process.env.rd_password ?? "";
 
-test.only("Test 1", async ({ page }) => {
-  console.log(rd_loginEmail);
+let rd_loginToken;
+
+test("Verify existing user login (using API)", async ({ page }) => {
+  //Create new API context to switch to API mode:
+  const APIContext = await request.newContext();
+
+  //Define the loginPayload, in URL-encoded data format.
+  //This will produce something like: "email=youremail%40example.com&password=yourpassword".
+  const loginPayload = new URLSearchParams();
+  loginPayload.append("email", rd_email);
+  loginPayload.append("password", rd_password);
+
+  //Send POST request to the "verifyLogin" endpoint, with the loginPayload.
+  //Sending it in the URL-encoded data format:
+  const loginResponse = await APIContext.post("https://automationexercise.com/api/verifyLogin", {
+    data: loginPayload.toString(),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  expect(loginResponse.ok()).toBeTruthy();
+  console.log(`Login Response Status: ${loginResponse.status()}`);
+
+  //Convert response to JSON format:
+  const loginResponse_JSON = await loginResponse.json();
+  console.log(loginResponse_JSON);
+
+  //Assert that the user exists:
+  expect(loginResponse_JSON.message).toBe("User exists!");
 });
