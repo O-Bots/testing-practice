795f5f6f82087a6e7818dfec4320bdeebaf00a7d
Okechi Williams <Okechi.williams@gmail.com>
2025-06-16 12:55:09 +0100

    .

---
+++ playwright/typescript/ow-login-func.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+import account from '../../ow_accountdetails.json';
+
+const baseURL = 'https://www.automationexercise.com/'
+
+console.log(account);
